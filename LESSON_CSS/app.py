9b0ba9b7648764615a3af530c35bf9d6c9526a95
Igor Volokho <igorvolokho99@gmail.com>
2024-07-22 13:14:11 +0300

    CSS WITH SOME CODE

---
+++ LESSON_CSS/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/table')
+def table():
+    data = [
+        {"date": "10.01.2010", "temperature": 30, "city": "Moscow"},
+        {"date": "10.01.2010", "temperature": 30, "city": "SPB"},
+        {"date": "10.01.2010", "temperature": 30, "city": "Rostov"},
+    ]
+    return render_template('table.html', data=data, image_name="cloud.png")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
